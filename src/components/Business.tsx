3cc0438e3db0f494182c2b05d466fab5116c9e18
Knoka1 <knoka1@hotmail.com>
2022-08-26 02:02:28 -0300

    (feat): Added structure to Business.tsx

--- src/components/Business.tsx
+++ src/components/Business.tsx
@@ -5,6 +5,29 @@ import Button from "./Button";
 
 import styles, { layout } from "../style";
 
+const FeatureCard = ({ icon, title, content, index }: any) => {
+  return (
+    <div
+      className={`flex flex-row p-6 rounded-[20px] ${
+        index !== features.length - 1 ? "mb-6" : "mb-0"
+      } feature-card`}
+    >
+      <div
+        className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
+      >
+        <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+      </div>
+      <div className="flex-1 flex flex-col ml-3">
+        <h4 className="font-poppings font-semibold text-white text-[18px] leading-[23px]">
+          {title}
+        </h4>
+        <p className="font-poppings font-normal text-dimWhite text-[16px] leading-[24px]">
+          {content}
+        </p>
+      </div>
+    </div>
+  );
+};
 const Business = () => {
   return (
     <section id="features" className={layout.section}>
@@ -12,11 +35,17 @@ const Business = () => {
         <h2 className={styles.heading2}>
           You have the brand, <br className="sm:block hidden" /> we have the
           operation.
-          <p className={`${styles.paragraph} max-w-[470px]`}>
-            Together our team will create with you a unique delivery, food
-            experience available to your customer
+          <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+            Together, we'll create a unique food delivery experience available
+            to your customer
           </p>
+          <Button styles="mt-10" />
         </h2>
+      </div>
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
       </div>
     </section>
   );
