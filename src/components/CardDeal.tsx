8092b3cf27d800ef2854e0e54d17a658f57904c0
Knoka1 <knoka1@hotmail.com>
2022-08-28 15:30:29 -0300

    (feat): Added CardDeal component

--- src/components/CardDeal.tsx
+++ src/components/CardDeal.tsx
@@ -1,7 +1,28 @@
 import React from "react";
 
+import Button from "./Button";
+import { card } from "../assets";
+
+import styles, { layout } from "../style";
+
 const CardDeal = () => {
-  return <div>CardDeal</div>;
+  return (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better deal <br className="sm:block hidden" /> in a few easy
+          steps.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Don't fall behind. Add a new operation to you business.
+        </p>
+        <Button styles="mt-10" />
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className="w-[100%] h-[100%]" />
+      </div>
+    </section>
+  );
 };
 
 export default CardDeal;
