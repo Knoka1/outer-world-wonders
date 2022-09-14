5d1decd7393050ae9442d0b35a4c6f6b09909742
Knoka1 <knoka1@hotmail.com>
2022-09-14 03:24:19 -0300

    (feat): Added home link to logo on Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,11 +10,13 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img
-        src={wow_logo}
-        alt="Wonders of the Outer World"
-        className="h-[124px] w-[97px]"
-      />
+      <Link to="/">
+        <img
+          src={wow_logo}
+          alt="Wonders of the Outer World"
+          className="h-[124px] w-[97px]"
+        />
+      </Link>
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => {
           return nav.id === "home" || nav.id === "login" ? (
